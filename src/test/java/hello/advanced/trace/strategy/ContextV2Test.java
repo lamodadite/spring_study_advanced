388e8bb55d6d007b4507a9338feb47e614106a6e
lamodadite <nmb11921@naver.com>
2024-12-20 19:47:53 +0900

    template pattern

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,21 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.ContextV2;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /*
+    전략 패턴 적용
+     */
+    @Test
+    void strategy() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+}
---
+++ src/test/java/hello/advanced/trace/template/template/Callback.java
@@ -0,0 +1,5 @@
+package hello.advanced.trace.template.template;
+
+public interface Callback {
+    void call();
+}
